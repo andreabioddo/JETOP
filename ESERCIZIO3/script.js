8e01d95387f0635e2727e64f1948f24bd1cf7e62
Biddo <86785608+andreabioddo@users.noreply.github.com>
2023-10-19 11:30:22 +0200

    Update script.js
    Modifiche

--- ESERCIZIO3/script.js
+++ ESERCIZIO3/script.js
@@ -12,34 +12,6 @@ const port = 3000;
 const weather = require('./routes/weather');
 app.use("/weather", weather);
 
-
-app.get('/es/:city/:day', async (req, res) => {
-        const city = req.params.city;
-        const day = req.params.day;
-  
-    try {
-      const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=API_KEY`);
-  
-      // Ottieni i dati dalla risposta dell'API
-      const weatherData = weatherResponse.data;
-  
-      // Altri placeholder per le altre API
-      const locationData = `Informazioni sulla posizione di ${city}`;
-      const otherData = `Altri dati per ${city} il giorno ${day}`;
-  
-      // Restituisci i dati come JSON
-      res.json({
-        city: city,
-        day: day,
-        weather: weatherData,
-        location: locationData,
-        other: otherData,
-      });
-    } catch (error) {
-      console.error('Errore durante la chiamata all\'API:', error);
-      res.status(500).json({ error: 'Errore nella richiesta all\'API' });
-    }
-  });
   
 app.listen(port, () => {
   console.log(`Il server è in ascolto sulla porta ${port}`);
